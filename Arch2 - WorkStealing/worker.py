fdd5a45b302d0473a20e489a25412587dc2ac0f2
mckennc9 <mckennc9@tcd.ie>
2017-12-07 18:04:11 +0000

    Added worker functionality
    Added registration ability to worker node, requesting work started

--- Arch2 - WorkStealing/worker.py
+++ Arch2 - WorkStealing/worker.py
@@ -7,4 +7,26 @@ from flask import request
 import json
 from pprint import pprint
 import requests
+from flask import Flask
+import pymongo
 
+application = Flask(__name__)
+
+#DB and server details
+serv_addr = "localhost"
+port = "27017"
+full_serv_addr = "http://127.0.0.1:5000"
+mongo_db_addr = "mongodb://" + serv_addr + ":" + port
+mongo_client = pymongo.MongoClient(mongo_db_addr)
+mongo_db = mongo_client.authServ	#connect in to auth db
+headers = {"Content-type": "application/json"}
+
+def registerWorkerNode():
+	requests.post(full_serv_addr + "/register", data=json.dumps({}), headers=headers)
+
+def requestWork():
+	requests.get(full_serv_addr + "/getWork", headers=headers)
+
+if __name__ == '__main__':
+	registerWorkerNode()
+	application.run()
